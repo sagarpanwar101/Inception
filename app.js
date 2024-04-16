9fd7f09cfbc56623f24bf24e5d5eb9523aa02dd6
sagarpanwar1 <sagarpanwar335@gmail.com>
2024-04-16 13:56:34 +0530

    final updates

--- app.js
+++ app.js
@@ -1,9 +1,32 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
 
-const heading = React.createElement("h1",{},"Hello world");
+const heading = React.createElement(
+    "h1",
+    {
+        id: "title",
+        style: {
+            background: "red",
+        },
+        className: "title"
+    },
+    "heading"
+);
+const heading1 = React.createElement(
+    "h1",
+    {
+        id: "title",
+    },
+    "heading1"
+);
 
-//passing react element inside root
-const root= ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
- 
+const container = React.createElement(
+    "div",
+    {
+        id: "container",
+    },
+    [heading, heading1]
+);
+
+// create root using createRoot
+const root = ReactDOM.createRoot(document.getElementById("root"));
+// passing react element inside root
+root.render(container);
